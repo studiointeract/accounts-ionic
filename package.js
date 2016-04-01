5071b68863280656104328c830c32d27eb78bd09
Tim Brandin <tim.brandin@gmail.com>
2016-04-01 08:41:26 +0200

    Updated.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  name: 'studiointeract:react-accounts-ui-semantic-ui',
-  version: '1.0.0',
+  name: 'std:react-accounts-ui-semantic-ui',
+  version: '1.1.0',
   summary: 'Semantic UI – Accounts UI for React in Meteor 1.3',
   git: 'https://github.com/studiointeract/react-accounts-ui-semantic-ui',
   documentation: 'README.md'
@@ -10,7 +10,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.3');
   api.use('ecmascript');
   api.use('underscore');
-  api.use('studiointeract:react-accounts-ui@1.0.13');
+  api.use('std:react-accounts-ui@1.1.0');
 
   api.mainModule('main.jsx');
 });
