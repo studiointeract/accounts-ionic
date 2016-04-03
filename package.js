e8381a7fc2e69d6ed2ef81ef05b6932c7de6cfe7
Tim Brandin <tim.brandin@gmail.com>
2016-04-03 13:52:46 +0200

    Upstream upgrades.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'std:accounts-semantic',
-  version: '1.1.2',
+  version: '1.1.3',
   summary: 'Semantic UI – Accounts UI for React in Meteor 1.3',
   git: 'https://github.com/studiointeract/accounts-semantic',
   documentation: 'README.md'
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   api.use('ecmascript');
   api.use('underscore');
   api.use('fourseven:scss@3.4.1');
-  api.use('std:accounts-ui@1.1.5');
+  api.use('std:accounts-ui@1.1.7');
 
   api.addFiles([
     'styles.scss'
