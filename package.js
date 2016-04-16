52ffeed3707b3049bf861c6346bec934a03700f7
Tim Brandin <tim.brandin@gmail.com>
2016-04-16 15:13:09 +0200

    Bump version of accounts-ui.

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   api.use('ecmascript');
   api.use('underscore');
   api.use('fourseven:scss@3.4.1');
-  api.use('std:accounts-ui@1.1.19');
+  api.use('std:accounts-ui@1.1.20');
   api.use('softwarerero:accounts-t9n@1.3.3');
 
   api.addFiles([
